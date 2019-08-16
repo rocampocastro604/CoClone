eae9fc4507b74050a2f3d2620da0bf0c2e007294
rocampocastro604 <44033716+rocampocastro604@users.noreply.github.com>
2019-08-16 16:33:54 -0500

    Reparando API

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,20 +1,43 @@
 const express = require('express')
 const router = express.Router()
+
 const Task = require('../models/task')
 
-//Aca se define la REST API
 router.get('/', async (req, res) => {
-    const tasks = await Task.find(function(err, tasks) {
-        
-        //Obtener tareas de la db
-        res.json(tasks)
-    })
-    router.post('/', async (req, res) => {
-        console.log(req.body)
-        res.json('recibido')
-    })
+    const tasks = await Task.find();
+    res.json(tasks);
+  });
+
+//Aca se define la REST API
+router.get('/:id', async (req, res) => {
+    const tasks = await Task.findById(req.params.id);
+    //Obtener tareas de la db
+        res.json(tasks);
+    });
+
+// ADD a new task
+router.post('/', async (req, res) => {
+    const { title, description } = req.body;
+    const task = new Task({title, description});
+    await task.save();
+    res.json({status: 'Task Saved'});
+  });
+
 /*     res.json({
         status: 'API FUNCIONANDO'
     }) */
-})
+
+// UPDATE a new task
+router.put('/:id', async (req, res) => {
+    const { title, description } = req.body;
+    const newTask = {title, description};
+    await Task.findByIdAndUpdate(req.params.id, newTask);
+    res.json({status: 'Task Updated'});
+  });
+  
+  router.delete('/:id', async (req, res) => {
+    await Task.findByIdAndRemove(req.params.id);
+    res.json({status: 'Task Deleted'});
+  });
+
 module.exports = router
